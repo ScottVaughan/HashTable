8f6d9a06d03e256475bb69e78a83e273a3b1f4fd
Scott <Scott@DESKTOP-L5UD7TV>
2022-06-13 19:00:28 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const ht = new HashTable();
+    ht.add("Canada", "300");
+    ht.add("Germany", "100");
+    ht.add("Italy", "50");
+    ht.add("United States", "300");
+    ht.add("France", "450");
+    ht.add("Mexico", "300");
+    ht.add("Brazil", "500");
+    ht.add("China", "400");
+    ht.add("Japan", "50");
+  });
+
   return (
     <div className="App">
     </div>
